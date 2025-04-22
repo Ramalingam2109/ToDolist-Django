91775ecc5a8c6379d95ac3f678126b09c62065bd
Ramalingam <ramalingam2109@outlook.com>
2025-04-22 09:46:42 +0530

    ready for deployment

--- todoproj/settings.py
+++ todoproj/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-i%%o01*h8d12xvq2-1*n4ilr6y$ddh3+f(exk3udp96ph#v$rw"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Ramalingam2109.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -116,8 +116,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Where files will be collected
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),  # Additional static folders
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
